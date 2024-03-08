a99166274715d9b8a55674c771d0d38343898e2d
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-03-08 12:53:09 +0100

    Con Metodi

--- DosStringhe.java
+++ DosStringhe.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
-public class DosStringhe {
+public class DosStringheConMetodos {
     public static void main(String[] args) {
-        String vocali="",consonanti="";
         Scanner in=new Scanner(System.in);
         System.out.println("Inserire una stringa");
         String s=in.nextLine();
+        System.out.println("La stringa di consonanti è: "+isConsonantes(s));
+        System.out.println("La stringa di vocali è: "+isVocales(s));
+
+    }
+    private static String isVocales(String s){
+        String vocali="";
         for (int i=0;i<s.length();i++){
             s=s.toLowerCase();
             if (Character.isLetter(s.charAt(i))){
@@ -17,13 +22,29 @@ public class DosStringhe {
                     case 'y':
                         vocali=vocali+s.charAt(i);
                         break;
+                }
+            }
+        }
+        return vocali;
+    }
+   private static String isConsonantes(String s){
+        String consonanti="";
+        for (int i=0;i<s.length();i++){
+            s=s.toLowerCase();
+            if (Character.isLetter(s.charAt(i))){
+                switch (s.charAt(i)){
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'y':
+                        break;
                     default:
                         consonanti=consonanti+s.charAt(i);
                 }
             }
         }
-        System.out.println("La stringa di consonanti è: "+consonanti);
-        System.out.println("La stringa di vocali è: "+vocali);
-
+        return consonanti;
     }
 }
