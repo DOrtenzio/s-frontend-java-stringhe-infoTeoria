997f883a3e426409080751ca20739a1f5ff31bf5
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-03-08 13:10:07 +0100

    Update DosStringhe 2

--- DosStringhe.java
+++ DosStringhe.java
@@ -1,50 +1,51 @@
 import java.util.Scanner;
-public class DosStringheConMetodos {
+public class DosStringhe {
     public static void main(String[] args) {
+        String vocali="",consonanti="";
         Scanner in=new Scanner(System.in);
         System.out.println("Inserire una stringa");
         String s=in.nextLine();
-        System.out.println("La stringa di consonanti è: "+isConsonantes(s));
-        System.out.println("La stringa di vocali è: "+isVocales(s));
+        for (int i=0;i<s.length();i++){
+            s=s.toLowerCase();
+            if (isVocal(s.charAt(i)))
+                vocali=vocali+s.charAt(i);
+            else if (isConsonant(s.charAt(i)))
+                consonanti=consonanti+s.charAt(i);
+        }
+        System.out.println("La stringa di consonanti è: "+consonanti);
+        System.out.println("La stringa di vocali è: "+vocali);
 
     }
-    private static String isVocales(String s){
-        String vocali="";
-        for (int i=0;i<s.length();i++){
-            s=s.toLowerCase();
-            if (Character.isLetter(s.charAt(i))){
-                switch (s.charAt(i)){
-                    case 'a':
-                    case 'e':
-                    case 'i':
-                    case 'o':
-                    case 'u':
-                    case 'y':
-                        vocali=vocali+s.charAt(i);
-                        break;
-                }
+    private static boolean isVocal(char t){
+        if (Character.isLetter(t)){
+            switch (t){
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y':
+                    return true;
+                default:
+                    return false;
             }
-        }
-        return vocali;
+        }else
+            return false;
     }
-   private static String isConsonantes(String s){
-        String consonanti="";
-        for (int i=0;i<s.length();i++){
-            s=s.toLowerCase();
-            if (Character.isLetter(s.charAt(i))){
-                switch (s.charAt(i)){
-                    case 'a':
-                    case 'e':
-                    case 'i':
-                    case 'o':
-                    case 'u':
-                    case 'y':
-                        break;
-                    default:
-                        consonanti=consonanti+s.charAt(i);
-                }
+    private static boolean isConsonant(char t){
+        if (Character.isLetter(t)){
+            switch (t){
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y':
+                    return false;
+                default:
+                    return true;
             }
-        }
-        return consonanti;
+        }else
+            return false;
     }
 }
